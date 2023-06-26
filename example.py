25074920980321d53f7826d52fd8432b476c06af
TeddyTodd <kevinmylinh.vermeulen@gmail.com>
2023-06-26 19:01:24 +0200

    remove API functions from example.py

--- example.py
+++ example.py
@@ -2,80 +2,19 @@
 import json
 
 import requests
-from metadata.ip2as import load_ip_to_asn
+import pyasn
 from processing.helpers import contains_interdomain_assume_symmetry
+from revtr_api.rest import fetch_revtr, run_revtrs
+
 
 API_KEY = ""
 # Please use pyasn at https://github.com/hadiasghari/pyasn. From the README of the page, use the following commands
 # pyasn_util_download.py --latest  # or --latestv46
 # pyasn_util_convert.py --single <Downloaded RIB File> <ipasn_db_file_name> and use <ipasn_db_file_name> below
-ip2asn = load_ip_to_asn("resources/bgpdumps/2023_05_05.dat")
+PYASN_DATA = "resources/pyasn_20230625.dat"
+ip2asn = pyasn.pyasn(str(PYASN_DATA))
 
-label_revtrs = "debug_tma"
-
-def get_sources():
-
-    """
-    Fetch the M-Lab sources that you can use to measure reverse traceroutes
-    from arbitrary destinations back to these sources.
-    :return:
-    """
-
-    url = "https://revtr.ccs.neu.edu/api/v1/sources"
-    headers = {
-        "Revtr-Key": API_KEY
-    }
-    results = requests.get(url, headers=headers)
-
-    return results
-
-def run_revtrs(source_destination_pair, label):
-
-    """
-    Run a list of reverse traceroutes with the given label
-    :param source_destination_pair:
-    :param label:
-    :return:
-    """
-
-    url = "https://revtr.ccs.neu.edu/api/v1/revtr"
-
-    headers = {
-        "Revtr-Key": API_KEY
-    }
-
-    revtrs = []
-    for source, destination in source_destination_pair:
-        revtrs.append({"src": source, "dst": destination, "label": label})
-    data = {
-        "revtrs": revtrs
-    }
-    results = requests.post(url=url, data=json.dumps(data), headers=headers)
-    print(results.json())
-    return results
-
-def fetch_revtr(batch_id, label):
-
-    """
-    Fetch the results of a batch of revtrs
-    :param batch_id:
-    :return:
-    """
-    # XOR
-    assert(bool(batch_id is not None) ^ bool(label != ""))
-    if batch_id is not None:
-        url = f"https://revtr.ccs.neu.edu/api/v1/revtr?batchid={batch_id}"
-    else:
-        url = f"https://revtr.ccs.neu.edu/api/v1/revtr?label={label}"
-
-    headers = {
-        "Revtr-Key": API_KEY
-    }
-    results = requests.get(url, headers=headers)
-    results_ = results.json()
-    if "revtrs" in results_:
-        return results_["revtrs"]
-    return []
+label_revtrs = "tma_round1_240_3"
 
 def print_revtrs(revtrs):
     for revtr in revtrs:
